cd874879d5d2b48b8af038eb881a20b68a6824ce
tudang2011994 <tudang2011994@gmail.com>
2026-09-01 14:10:24 -0700

    commit file buildspec.yml

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from pydantic import BaseModel
+import os
+
+# Force Hugging Face to accept remote code without running the timer
+os.environ["HF_TRUST_REMOTE_CODE"] = "1" 
 
 MODEL_ID = 'AxiomicLabs/GPT-X2.5-135M'
 
@@ -9,6 +13,7 @@ MODEL_ID = 'AxiomicLabs/GPT-X2.5-135M'
 tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
 model = AutoModelForCausalLM.from_pretrained(
     MODEL_ID,
+    trust_remote_code=True,
     device_map = "auto"
     )
 
@@ -18,6 +23,10 @@ app = FastAPI()
 class Request(BaseModel):
     text: str
 
+@app.get("/health")
+def check_health():
+    return "Good"
+
 @app.post("/chat")
 def chat(request: Request ):
 
@@ -25,17 +34,17 @@ def chat(request: Request ):
 
     tokens = tokenizer(
         user_request,
-        return_tensor = "pt"               
+        return_tensors = "pt"               
         ).to(model.device)
     
     outputs = model.generate(
         **tokens,
-        max_new_token = 100
+        max_new_tokens = 100
         )
 
     response = tokenizer.decode(
         outputs[0],
-        skip_special_tokens= "true"
+        skip_special_tokens= True
         )
 
     return {"response": response}
